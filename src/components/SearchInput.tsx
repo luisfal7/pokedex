081b470dab776516a84fec5330be845ba8f66dba
Luis Falconi <luism.falconi@gmail.com>
2023-01-25 17:56:47 -0300

    add debounce searchScreen

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,68 +1,71 @@
-import React, {useState, useEffect} from 'react'
-import { View, StyleSheet, TextInput, StyleProp, ViewStyle } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
 import Icon from "@expo/vector-icons/Ionicons";
-import { useDebouncedValue } from '../hooks/useDebouncedValue';
+import { useDebouncedValue } from "../hooks/useDebouncedValue";
 
 interface Props {
-    style?: StyleProp<ViewStyle>
+  onDebounce: ( value: string ) => void;
+  style?: StyleProp<ViewStyle>;
 }
 
-export const SearchInput = ({ style }: Props) => {
+export const SearchInput = ({ style, onDebounce }: Props) => {
+  const [textValue, setTextValue] = useState("");
 
-    const [textValue, setTextValue] = useState('')
+  const deboncedValue = useDebouncedValue(textValue, 600);
 
-    const deboncedValue = useDebouncedValue(textValue, 600)
+  useEffect(() => {
 
-    useEffect(() => {
-        deboncedValue
-    }, [deboncedValue])
+    onDebounce(deboncedValue);
     
+  }, [deboncedValue]);
 
   return (
-    <View style={{ ...styles.container, ...style as any }}>
-        <View style={ styles.textBackgroud }>
-            <TextInput 
-            placeholder='Buscar pokémon'
-            style={ styles.textInput }
-            autoCapitalize='none'
-            autoCorrect={false}
-            value={ textValue }
-            onChangeText={setTextValue}
-            />
-            <Icon 
-                name='search-outline'
-                color='gray'
-                size={30}
-            />
-        </View>
+    <View style={{ ...styles.container, ...(style as any) }}>
+      <View style={styles.textBackgroud}>
+        <TextInput
+          placeholder="Buscar pokémon"
+          style={styles.textInput}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={textValue}
+          onChangeText={setTextValue}
+        />
+        <Icon name="search-outline" color="gray" size={30} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        //backgroundColor: 'red'
+  container: {
+    //backgroundColor: 'red'
+  },
+  textBackgroud: {
+    backgroundColor: "#e0d7d7",
+    borderRadius: 50,
+    height: 40,
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    textBackgroud: {
-        backgroundColor: '#e0d7d7',
-        borderRadius: 50,
-        height: 40,
-        paddingHorizontal:20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
-        elevation: 5,
-    },
-    textInput: {
-        flex: 1,
-        fontSize: 18
-    }
-})
+    elevation: 5,
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,64 +1,73 @@
-import React from "react";
-import { View, Text, FlatList, Dimensions } from 'react-native';
+import React,{useState, useEffect} from "react";
+import { View, Text, FlatList, Dimensions } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Loading } from "../components/Loading";
 import { PokemonCard } from "../components/PokemonCard";
 import { SearchInput } from "../components/SearchInput";
-import { usePokemonSearch } from '../hooks/usePokemonSearch';
-import { styles } from '../theme/AppTheme';
+import { usePokemonSearch } from "../hooks/usePokemonSearch";
+import { SimplePokemon } from "../interfaces/PokemonInterfaces";
+import { styles } from "../theme/AppTheme";
 
-const screenWidth = Dimensions.get('window').width
+const screenWidth = Dimensions.get("window").width;
 
 export const SearchScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const { isFetching, simplePokemonList } = usePokemonSearch();
+  const [term, setTerm] = useState('')
+  const [pokemonFiltered, setPokemonFiltered] = useState<SimplePokemon[]>([])
 
-  const { top } = useSafeAreaInsets()
-  const { isFetching, simplePokemonList } = usePokemonSearch()
+  useEffect(() => {
+    
+    if( term.length === 0 ){
+      return setPokemonFiltered([])
+    }
 
-  if(isFetching){
-    return(
-      <Loading />
+    setPokemonFiltered(
+      simplePokemonList.filter( poke => poke.name.toLowerCase().includes( term.toLowerCase() ) )
     )
+
+  }, [term])
+  
+
+  if (isFetching) {
+    return <Loading />;
   }
 
   return (
     <View style={{ flex: 1, marginTop: top + 10, marginHorizontal: 20 }}>
-
-      <SearchInput 
+      <SearchInput
+        onDebounce = { (value) => setTerm(value) }
         style={{
           position: "absolute",
-          opacity:0.92,
+          opacity: 0.92,
           zIndex: 999,
           width: screenWidth - 40,
-          top: top + 10
+          top: top + 10,
         }}
       />
 
-      <FlatList 
-          //data
-          data={simplePokemonList}
-          keyExtractor = { (pokemon) => pokemon.id  }
-          renderItem = {({ item, index }) => ( <PokemonCard pokemon={ item }/> )}
-          numColumns= {2}
-
-          ListHeaderComponent={(
-            <Text style={{ 
-              ...styles.title, 
+      <FlatList
+        //data
+        data={pokemonFiltered}
+        keyExtractor={(pokemon) => pokemon.id}
+        renderItem={({ item, index }) => <PokemonCard pokemon={item} />}
+        numColumns={2}
+        ListHeaderComponent={
+          <Text
+            style={{
+              ...styles.title,
               ...styles.globalMargin,
               top: 10,
-              paddingBottom:10,
-              marginTop: top + 40 
-            }}>
-              Pokedex
-            </Text>
-          )}
-
-          //Indicator
-          showsVerticalScrollIndicator={false}
-
-        />
-
+              paddingBottom: 10,
+              marginTop: top + 40,
+            }}
+          >
+            { term }
+          </Text>
+        }
+        //Indicator
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
-
-
